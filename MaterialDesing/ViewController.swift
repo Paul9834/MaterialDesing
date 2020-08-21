ce878f015e65d274cef4aa212ea93bda174fefe5
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-08-21 00:22:17 -0500

    update

--- MaterialDesing/ViewController.swift
+++ MaterialDesing/ViewController.swift
@@ -11,8 +11,21 @@ import MaterialComponents.MaterialTextFields
 
 class ViewController: UIViewController {
 
+    var textController : MDCTextInputControllerOutlined!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let textFieldFloating = MDCTextField(frame: CGRect(x: 0, y: 20, width: self.view.frame.width - 50, height: 50))
+        textFieldFloating.placeholder =  "Nombre"
+        textFieldFloating.center = self.view.center
+
+
+        self.view.addSubview(textFieldFloating)
+
+
+        self.textController = MDCTextInputControllerOutlined(textInput: textFieldFloating)
+        self.textController.textInsets(UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16) )
 
 
 
